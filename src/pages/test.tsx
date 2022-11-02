e0b1031b4d12fe01d545c508b7002137431ef542
Anirbanjyoti <anirbanjyoti708@gmail.com>
2022-11-02 21:06:44 +0530

    #8

---
+++ src/pages/test.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+type testProps = {
+    
+};
+
+const test:React.FC<testProps> = () => {
+    
+    return (
+    <>
+    Have a good coding
+    </>
+    )
+}
+export default test;
